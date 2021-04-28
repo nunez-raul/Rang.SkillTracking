811b049c2a659b2e75df7c208d6c31f48e2f16b2
raul.nunez <nunez.raul@gmail.com>
2021-04-28 12:12:36 -0500

    Added the first test class

---
+++ Rang.SkillTracking.Tests.xUnit/Rang.SkillTracking.Tests.xUnit.Domain/SetSkillSnapshotTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rang.SkillTracking.Tests.xUnit.Rang.SkillTracking.Tests.xUnit.Domain
+{
+    public class SetSkillSnapshotTests
+    {
+        [Fact]
+        public void SetSkillSnapshot()
+        {
+            // arrange
+            // act
+            // assert
+        }
+    }
+}
