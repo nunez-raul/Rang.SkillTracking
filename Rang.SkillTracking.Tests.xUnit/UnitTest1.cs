e1665c294a370e919047e84ab9ea4959545eae63
raul.nunez <nunez.raul@gmail.com>
2021-04-27 19:32:47 -0500

    Add project files.

---
+++ Rang.SkillTracking.Tests.xUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Rang.SkillTracking.Tests.xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
