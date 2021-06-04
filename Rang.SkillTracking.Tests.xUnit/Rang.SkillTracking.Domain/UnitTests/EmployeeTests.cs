06377ed443b86d608f89c2de3d86e47232140c1c
raul.nunez <nunez.raul@gmail.com>
2021-06-04 13:13:23 -0500

    Added EmployeeTests

---
+++ Rang.SkillTracking.Tests.xUnit/Rang.SkillTracking.Domain/UnitTests/EmployeeTests.cs
@@ -0,0 +1,23 @@
+﻿using Rang.SkillTracking.Domain.Employees;
+using Xunit;
+
+namespace Rang.SkillTracking.Tests.xUnit.Rang.SkillTracking.Domain.UnitTests
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void ShouldCreateInstance()
+        {
+            // arrange
+
+            // act
+            var result = new Employee();
+
+            // assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Profile);
+            Assert.NotNull(result.Evaluatee);
+            Assert.NotNull(result.SkillEvaluator);
+        }
+    }
+}
